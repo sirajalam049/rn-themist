e52302e9ed8b34f987cbf9edf744ab9826a881b7
Siraj Alam <sirajalam049@gmail.com>
2019-07-06 23:52:22 +0530

    Compiled result deleted from main directory

--- src/ThemeProvider/ThemeProvider.d.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from 'react';
-import { RNTheme } from '../RNTheme';
-export interface ThemeProviderProps {
-    theme: Partial<RNTheme>;
-}
-declare class ThemeProvider extends Component<ThemeProviderProps> {
-    render(): JSX.Element;
-}
-export default ThemeProvider;
