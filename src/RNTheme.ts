cc7e580c41d92996feb61359a050f551d9ba7227
Siraj Alam <sirajalam049@gmail.com>
2019-07-07 17:36:01 +0530

    Typography component started

--- src/RNTheme.ts
+++ src/RNTheme.ts
@@ -1,13 +1,14 @@
+import { TypographyVariants } from "./components/Typography/Typography";
+import { StyleProp, TextStyle } from "react-native";
+
+export type ThemeColors = 'primary' | 'secondary' | 'textPrimary' | 'textSecondary' | 'contrast' | 'error' | 'success' | 'primaryAccent' | 'secondaryAccent'
+
 export interface RNTheme {
-    Color: {
-        primary: string
-        secondary: string
-        textPrimary: string
-        textSecondary: string
-        contrast: string
-        primaryAccent: string
-        secondaryAccent: string
-    },
+    Color: Record<ThemeColors, string>,
+    Typography: {
+        variant: Record<TypographyVariants, StyleProp<TextStyle>>,
+        gutterBottom: number
+    }
 
 }
 
@@ -18,12 +19,33 @@ const DefaultTheme: RNTheme = {
         textPrimary: '#5A5A5A',
         textSecondary: '#5E6368',
         contrast: '#FFFFF',
-        get primaryAccent() {
+        error: '',
+        success: '',
+        get primaryAccent(): string {
             return this.primary
         },
-        get secondaryAccent() {
+        get secondaryAccent(): string {
             return this.secondary
         }
+    },
+    Typography: {
+        variant: {
+            h1: {},
+            h2: {},
+            h3: {},
+            h4: {},
+            h5: {},
+            h6: {},
+            body1: {},
+            body2: {},
+            button: {},
+            caption: {},
+            error: {},
+            overline: {},
+            subtitle1: {},
+            subtitle2: {}
+        },
+        gutterBottom: 20
     }
 }
 
