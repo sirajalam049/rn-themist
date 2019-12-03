bc33b2ec54e01c8c11ae501f463864bbd9df9fa4
Siraj Alam <sirajalam049@gmail.com>
2019-12-03 18:52:55 +0530

    make styles typings improved

--- src/styles/makeStyles/makeStyles.ts
+++ src/styles/makeStyles/makeStyles.ts
@@ -7,7 +7,7 @@ export type StylesHook<T> = () => StyleSheet.NamedStyles<T>;
 
 export type StyleFunction<T> = (theme: RNTheme) => StyleSheet.NamedStyles<T>;
 
-export default function makeStyles<T>(styleFunction: StyleFunction<T>): StylesHook<T> {
+export default function makeStyles<T extends StyleSheet.NamedStyles<T> = StyleSheet.NamedStyles<any>>(styleFunction: StyleFunction<T>): StylesHook<T> {
     return (() => {
         const theme = React.useContext<RNTheme>(ThemeContext);
         return styleFunction(theme);
